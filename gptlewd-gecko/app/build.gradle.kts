3921c67eb09f55bc6d84067518d434ff14bf59e2
ximbica12 <110624363+ximbica12@users.noreply.github.com>
2026-09-23 20:58:59 -0300

    Compile GPTLewd Gecko against Android 37

--- gptlewd-gecko/app/build.gradle.kts
+++ gptlewd-gecko/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.ximbica.gptlewd.gecko"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.ximbica.gptlewd.gecko"
